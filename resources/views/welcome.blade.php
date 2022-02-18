12ef54e2aa233e2418e098a9478cf9be2f6c1683
Saniyat Al Ahmed <saniyat013@gmail.com>
2022-02-18 11:46:41 +0600

    Initialization of Web Report Backend

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Web Report Backend</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+            text-align: center;
+            margin-top: 100px;
+        }
+
+    </style>
+</head>
+
+<body class="antialiased">
+    <div>
+        <h1>Welcome to online balag reporting system.</h1>
+        <h3>
+            This is the backend of the application.
+            Please <a href="http://wrf.saniyat.com/">click here</a> to visit the main website
+        </h3>
+    </div>
+</body>
+
+</html>
